85e89a3defbe73612fd1d801c3cf6313132d247c
lauri3new <laurence@wearepercent.com>
2020-11-24 22:30:50 +0000

    more methods

--- src/main/scala/Arrow.scala
+++ src/main/scala/Arrow.scala
@@ -6,52 +6,81 @@ import Either._
 import Right._
 
 object Arrow {
-  def apply[D, E, R](v: D => Future[Either[E, R]]) = {
-    println("apply run")
-    new Arrow(v)
-  }
-  def resolve[D, R](v: R) = {
-    new Arrow((d: D) => Future(Right(v)))
-  }
-  def reject[D, R](v: R) = {
-    new Arrow((d: D) => Future(Left(v)))
-  }
+  def apply[D, E, R](v: D => Future[Either[E, R]]) = new Arrow(v)
+
+  def resolve[D, R](v: R) = new Arrow((d: D) => Future(Right(v)))
+
+  def reject[D, R](v: R) = new Arrow((d: D) => Future(Left(v)))
 }
 
 class Arrow[-D, E, R](val v: D => Future[Either[E, R]]) {
   def map[R2](f: R => R2) = Arrow((d:D) => v(d).map(ea => ea.map(f)))
+
   def flatMap[D2 <: D, E1, R2](f: R => Arrow[D2, E1, R2]) = Arrow((d:D2) => v(d).flatMap(
   (ea) => ea match {
     case Left(value) => Future(Left(value))
     case Right(value) => f(value).v(d)
     }
   ))
+
   def leftMap[E2](f:E => E2) = Arrow((d:D) => v(d).map(ea => ea.left.map(f)))
+
   def biMap[E2, R2](f:E => E2, g: R => R2) = Arrow((d:D) => v(d).map(ea => ea match {
     case Left(value) => Left(f(value))
     case Right(value) => Right(g(value))
   }))
+
   def orElse[D2 <: D, E2, R2](f:Arrow[D2, E2, R2]) = Arrow((d:D2) => v(d).flatMap(ea => ea match {
     case Left(value) => f.v(d)
     case Right(value) => Future(Right(value))
   }))
+
   def andThen[R2, E1](f: Arrow[R, E1, R2]) = Arrow((d:D) => v(d).flatMap(
     (ea) => ea match {
       case Left(value) => Future(Left(value))
       case Right(value) => f.v(value)
     }
   ))
+
   def group[D2 <: D, E2, R2](f:Arrow[D2, E2, R2]) = Arrow((d:D2) => v(d).flatMap(
     (ea) => ea match {
       case Left(value) => Future(Left(value))
       case Right(value) => f.v(d).map(eb => eb.map(r => (value, r)))
     }
   ))
+
+  def groupFirst[D2 <: D, E2, R2](f:Arrow[D2, E2, R2]) = Arrow((d:D2) => v(d).flatMap(
+    (ea) => ea match {
+      case Left(value) => Future(Left(value))
+      case Right(value) => f.v(d)
+    }
+  ))
+
+  def groupSecond[D2 <: D, E2, R2](f:Arrow[D2, E2, R2]) = Arrow((d:D2) => v(d).flatMap(
+    (ea) => ea match {
+      case Left(value) => Future(Left(value))
+      case Right(value) => f.v(d).map(eb => eb.map(r => value))
+    }
+  ))
+
+  def groupParallel[D2 <: D, E2, R2](f:Arrow[D2, E2, R2]): Arrow[D2, E | E2, (R, R2)] = Arrow((d:D2) => {
+    val a = v(d)
+    val b = f.v(d)
+    val x = for {
+      a1 <- a
+      b1 <- b
+    } yield ((a1, b1))
+    x.map(
+      (c, d) => c.flatMap(r1 => d.map(r2 => (r1, r2))) 
+    )
+  })
+
   def flatMapF[D2 <: D, E1, R2](f: R => D2 => Future[Either[E, R]]) = Arrow((d:D2) => v(d).flatMap(
     (ea) => ea match {
       case Left(value) => Future(Left(value))
       case Right(value) => f(value)(d)
     }
   ))
+
   def runAsFuture(d:D) = v(d)
 }
--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -8,39 +8,34 @@ object Main {
     trait user {
       def a: String
     }
-    trait noob {
+    trait account {
       def b: Int
     }
 
     case class User(a: String) extends user
-    case class Noob(b: Int) extends noob
-
-    enum Bla {
-      case X, Y
-    }
+    case class Account(b: Int) extends account
 
     trait userService {
       def userService: user
     }
-    trait noobService {
-      def noobService: noob
+    trait accountService {
+      def AccountService: account
     }
 
-    case class Services(userService: user, noobService: noob) extends userService with noobService
+    case class Services(userService: user, AccountService: account) extends userService with accountService
 
     object service {
       def get(a: String) = Arrow((n:userService) => Future(Right(n.userService.a)))
     }
-    val x = Arrow.resolve[noobService, Int](5)
+    val x = Arrow.resolve[accountService, Int](5)
       .flatMap(
         (a) => service.get(a.toString)
       )
       .map(a => {
-      println("huh")
+      println("test")
       a
-    }).runAsFuture(Services(User("yo"), Noob(4)))
-    val y = Future(5)
-    println("Hello world!")
+    })
+    x.groupParallel(x).runAsFuture(Services(User("yo"), Account(4)))
   }
 
   def msg = "Arrow"
