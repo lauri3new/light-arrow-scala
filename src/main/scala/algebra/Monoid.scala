3b8865bb43f3a9a5a5efb3929f77989f49f04718
lauri3new <laurence@wearepercent.com>
2021-01-17 21:36:14 +0000

    motivating

--- src/main/scala/algebra/Monoid.scala
+++ src/main/scala/algebra/Monoid.scala
@@ -1,5 +1,39 @@
 package algebra
 
+// motivating example
+
+val x = List("bob").concat("loblaw")
+val y = "bob" + "loblaw"
+
+def repeatString (n: Int)(s: String): String = {
+  def repeat(n2: Int)(s2: String): String = {
+    if (n2 <= 1) {
+      s2
+    } else {
+      repeat(n2 - 1)(s2 + s)
+    }
+  }
+  repeat(n)(s)
+}
+
+def repeatList [A](n: Int)(as: List[A]): List[A] = {
+  def repeat(n2: Int)(as2: List[A]): List[A] = {
+    if (n2 <= 1) {
+      as2
+    } else {
+      repeat(n2 - 1)(as.concat(as2))
+    }
+  }
+  repeat(n)(as)
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(repeatString(5)("ok"))
+    println(repeatList(5)(List(2)))
+  }
+}
+
 // Monoids : things we can combine together
 
 trait Monoid[A] {
@@ -9,7 +43,7 @@ trait Monoid[A] {
 
 // WHERE empty should hold true for this law:
 // combine(empty, A) === combine(empty, a) === a
-// can't encode this to be checked by compiler in Scala, but could do in other languages
+// and associative: combine(combine(A, B), C) === combine(A, combine(B, C))
 
 // scala 3 stuff
 object Monoid {
@@ -26,23 +60,38 @@ given Monoid[Int] with
   def combine (x:Int, y: Int): Int = x + y
   def empty: Int = 0
 
+given [A]: Monoid[List[A]] with
+  def combine (x:List[A], y: List[A]): List[A] = x.concat(y)
+  def empty: List[A] = List()
+
 // Monoid combinators (combinator = function with no free variables)
 // this is the payoff :)
+
+def repeat[T: Monoid](n:Int)(x: T) = {
+  def repeat(n2: Int)(y: T): T = {
+    if (n2 <= 1) {
+      y
+    } else {
+      repeat(n2 - 1)(Monoid[T].combine(y, x))
+    }
+  }
+  repeat(n)(x)
+}
    
 def combineAll[T: Monoid](
   xs: List[T]
 ): T = xs.foldLeft(Monoid[T].empty)((x, y) => Monoid[T].combine(x, y))
 
-val strings = List("yo", "wasup", "hi")
-val numbers = List(1, 5, 3)
 
-val myString = combineAll(strings)
-val myNumber = combineAll(numbers)
+// val strings = List("yo", "wasup", "hi")
+// val numbers = List(1, 5, 3)
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println(myString)
-    println(myNumber)
-  }
-}
+// val myString = combineAll(strings)
+// val myNumber = combineAll(numbers)
 
+// object Main {
+//   def main(args: Array[String]): Unit = {
+//     println(repeatString(5)("ok"))
+//     println(repeatList(5)(List(2)))
+//   }
+// }
